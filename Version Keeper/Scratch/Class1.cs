71bffc6f62a9b2720d084ef7afb7d642cf931391
Dafydd Giddins <dafydd.giddins@wonga.com>
2014-09-27 13:56:17 +0100

    Initial commit, contains broken tests while moving code from one location to another

---
+++ Version Keeper/Scratch/Class1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using NUnit.Framework;
+using Semver;
+
+namespace Scratch
+{
+    [TestFixture]
+    public class AddingAnApplicationTests
+    {
+        [Test]
+        public void CreatingANewApplicationCallsTheStateStore()
+        {
+            var stateStoreSpy = new VersionApplicationStateStoreMock();
+
+            var classUnderTest = new VersionKeeper(stateStoreSpy);
+            classUnderTest.AddVersionedApplication("TestApplication", "Application Description");
+
+            stateStoreSpy.AssertThisApplicationWasCreated("TestApplication", "Application Description");
+        }
+
+        [Test]
+        public void AVersionedApplicationIsReturedWhenCreated()
+        {
+            var stateStoreSpy = new VersionApplicationStateStoreMock();
+
+            var classUnderTest = new VersionKeeper(stateStoreSpy);
+            VersionedApplication versionedApplication = classUnderTest.AddVersionedApplication("TestApplication", "Application Description");
+
+            Assert.That(versionedApplication, Is.Not.Null);
+        }
+
+        [Test]
+        public void WhenApplicationWithSameNameExistsReturnsDuplicateResult()
+        {
+            var state = new VersionedApplicationState("test", "description");
+            var stateStoreSpy = new VersionApplicationStateStoreMock();
+            stateStoreSpy.SetupApplicationStateToReturn(state);
+
+            var classUnderTest = new VersionKeeper(stateStoreSpy);
+
+            var existingApplication = classUnderTest.AddVersionedApplication("test", "other description");
+            Assert.That(existingApplication, Is.TypeOf<ExistingVersionedApplication>());
+        }
+    }
+
+    [TestFixture]
+    public class RetrievingAnApplicationTests
+    {
+
+        [Test]
+        public void CanRetrieveAnApplicationWhenItIsTheStateStore()
+        {
+            const string applicationDescription = "description";
+            const string applicationName = "test";
+
+            var stateStore = new VersionApplicationStateStoreMock();
+            stateStore.SetupApplicationStateToReturn(new VersionedApplicationState(applicationName, applicationDescription));
+
+            var classUnderTest = new VersionKeeper(stateStore);
+            VersionedApplication application = classUnderTest.GetVersionedApplication(applicationName);
+
+            Assert.That(application, Is.Not.Null);
+            Assert.That(application.Name, Is.EqualTo(applicationName));
+            Assert.That(application.Description, Is.EqualTo(applicationDescription));
+
+            stateStore.AssertThisApplicationNameWasRequested(applicationName);
+        }
+
+        [Test]
+        public void WhenApplicationDoesNotExistANullApplicationIsReturned()
+        {
+            var classUnderTest = new VersionKeeper(new VersionApplicationStateStoreMock());
+            VersionedApplication application = classUnderTest.GetVersionedApplication("applicationName");
+
+            Assert.That(application, Is.TypeOf<NullVersionedApplication>());
+
+            Assert.That(application.Name, Is.Empty);
+            Assert.That(application.Description, Is.Empty);
+        }
+    }
+
+
+    [TestFixture]
+    public class VersioningApplicationTests
+    {
+        [Test]
+        public void TheDefaultVersionForAnApplicaitonIs_0_0_0_0()
+        {
+            var classUnderTest = new VersionKeeper(new VersionApplicationStateStoreMock());
+            VersionedApplication application = classUnderTest.AddVersionedApplication("name", "desc");
+
+            Assert.That(application.Version, Is.TypeOf<SemVersion>());
+            Assert.That(application.Version.ToString(), Is.EqualTo("0.0.0"));
+        }
+
+        [Test]
+        public void CanCreateAnApplicationAtASpecificVersion()
+        {
+            const string setVersion = "1.2.3-4";
+            var classUnderTest = new VersionKeeper(new VersionApplicationStateStoreMock());
+            VersionedApplication application = classUnderTest.AddVersionedApplication("name", "desc",
+                SemVersion.Parse(setVersion));
+
+            Assert.That(application.Version.ToString(), Is.EqualTo(setVersion));
+        }
+    }
+
+    [TestFixture]
+    public class RecordingBuildsForANewApplication
+    {
+        [TestCase(1, "1")]
+        [TestCase(2, "2")]
+        [TestCase(3, "3")]
+        [TestCase(23, "23")]
+        public void BuildNumberReflectsTheNumberOfRecordedBuildsForAVersionControlIdentifer(int numberOfBuildsRecorded, string expectedBuildNumber)
+        {
+            const string applicationName = "test";
+
+            var classUnderTest = BuildUpClassWithNewApplication(applicationName);
+            var application = classUnderTest.GetVersionedApplication("test");
+
+            string versionControlChangeIdentifier = Guid.NewGuid().ToString();
+
+            for (int i = 0; i < numberOfBuildsRecorded; i++)
+                application.RecordBuild(versionControlChangeIdentifier);    
+
+            Assert.That(application.Version.Build, Is.EqualTo(expectedBuildNumber));
+        }
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(8)]
+        [TestCase(23)]
+        public void RecordingMultipleBuildsWithDifferentVersionControlIdentifiersDoesNotIncrementBuild(int numberOfBuildsRecorded)
+        {
+            const string applicationName = "test";
+
+            var classUnderTest = BuildUpClassWithNewApplication(applicationName);
+
+            var application = classUnderTest.GetVersionedApplication(applicationName);
+            
+            for (int i = 0; i < numberOfBuildsRecorded; i++)
+            {
+                application.RecordBuild(GenerateRandomVersionControlIdentifier());
+                Assert.That(application.Version.Build, Is.EqualTo("1"));
+            }
+        }
+
+        [Test]
+        public void RecordingDifferentBuildIdentifiersIncrementsThePatchNumber()
+        {
+            const string applicationName = "test";
+
+            var classUnderTest = BuildUpClassWithNewApplication(applicationName);
+
+            var application = classUnderTest.GetVersionedApplication(applicationName);
+
+            application.RecordBuild(GenerateRandomVersionControlIdentifier());
+            Assert.That(application.Version.Patch, Is.EqualTo(1));
+
+            application.RecordBuild(GenerateRandomVersionControlIdentifier());
+            Assert.That(application.Version.Patch, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void TestCombinations()
+        {
+            const string applicationName = "test";
+
+            var classUnderTest = BuildUpClassWithNewApplication(applicationName);
+
+            var application = classUnderTest.GetVersionedApplication(applicationName);
+
+            string firstVersionControlNumber = GenerateRandomVersionControlIdentifier();
+            application.RecordBuild(firstVersionControlNumber);
+            application.RecordBuild(firstVersionControlNumber);
+
+            string secondVersionControlNumber = GenerateRandomVersionControlIdentifier();
+
+            application.RecordBuild(secondVersionControlNumber);
+            application.RecordBuild(secondVersionControlNumber);
+
+            Assert.That(application.Version.ToString(), Is.EqualTo("0.0.2+2"));
+        }
+
+        [Test]
+        public void CannotRecordBuildsForAVersionControlIdentifierIfItIsNotTheMostRecentAndHasAlreadyBeenRecorded()
+        {
+            const string applicationName = "test";
+
+            var classUnderTest = BuildUpClassWithNewApplication(applicationName);
+
+            var application = classUnderTest.GetVersionedApplication(applicationName);
+
+            string firstVersionControlNumber = GenerateRandomVersionControlIdentifier();
+            string secondVersionControlNumber = GenerateRandomVersionControlIdentifier();
+
+            application.RecordBuild(firstVersionControlNumber);
+            application.RecordBuild(secondVersionControlNumber);
+            application.RecordBuild(firstVersionControlNumber);
+        }
+
+        [Test]
+        public void CanGetTheHighestVersionNumberForVersionControlId()
+        {
+
+        }
+
+        [Test]
+        public void VersionNumberIncrementedCorrectlyAccrossInstancesOfVersionKeeper()
+        {
+            const string applicationName = "test";
+
+            var stateStore = new VersionApplicationStateStoreMock();
+
+            VersionKeeper firstInstanceOfVersionKeeper = BuildUpClassWithNewApplication(applicationName, stateStore);
+            VersionedApplication application = firstInstanceOfVersionKeeper.GetVersionedApplication(applicationName);
+
+            string firstVersionControlNumber = GenerateRandomVersionControlIdentifier();
+            application.RecordBuild(firstVersionControlNumber);
+            application.RecordBuild(firstVersionControlNumber);
+
+            string secondVersionControlNumber = GenerateRandomVersionControlIdentifier();
+
+            application.RecordBuild(secondVersionControlNumber);
+            application.RecordBuild(secondVersionControlNumber);
+
+            var secondInstanceOfVersionKeeper = new VersionKeeper(stateStore);
+            application = secondInstanceOfVersionKeeper.GetVersionedApplication(applicationName);
+
+            Assert.That(application.Version.ToString(), Is.EqualTo("0.0.2+2"));
+
+            application.RecordBuild(firstVersionControlNumber);
+            application.RecordBuild(firstVersionControlNumber);
+
+            Assert.That(application.Version.ToString(), Is.EqualTo("0.0.1+4"));
+        }
+        
+        private static VersionKeeper BuildUpClassWithNewApplication(string applicationName, VersionApplicationStateStoreMock stateStore = null)
+        {
+            if (stateStore == null)
+                stateStore = new VersionApplicationStateStoreMock();
+
+            var versionedApplicationState = new VersionedApplicationState(applicationName, "");
+            versionedApplicationState.CurrentVersion = new SemVersion(new Version());
+            stateStore.SetupApplicationStateToReturn(versionedApplicationState);
+            var classUnderTest = new VersionKeeper(stateStore);
+            return classUnderTest;
+        }
+
+        private static string GenerateRandomVersionControlIdentifier()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+
+
+    public class VersionApplicationStateStoreMock : IVersionApplicationStateStore
+    {
+        private string _applicationName;
+        private string _applicationDescription;
+        
+        public void Write(VersionedApplicationState state)
+        {
+            _applicationName = state.ApplicationName;
+            _applicationDescription = state.ApplicationDescription;
+        }
+
+        private string _applicationStateRequested;
+        private VersionedApplicationState _applicationStateToReturn;
+        public VersionedApplicationState Read(string applicationName)
+        {
+            _applicationStateRequested = applicationName;
+            return _applicationStateToReturn;
+        }
+
+        public void AssertThisApplicationWasCreated(string applicationName, string applicationDescription)
+        {
+            Assert.That(_applicationName, Is.EqualTo(applicationName));
+            Assert.That(_applicationDescription, Is.EqualTo(applicationDescription));
+        }
+
+        public void AssertThisApplicationNameWasRequested(string applicationName)
+        {
+            Assert.That(_applicationStateRequested, Is.EqualTo(applicationName));
+        }
+
+        public void SetupApplicationStateToReturn(VersionedApplicationState applicationState)
+        {
+            _applicationStateToReturn = applicationState;
+        }
+    }
+}
---
+++ Version Keeper/Scratch/VersionKeeper.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using Semver;
+
+namespace Scratch
+{
+    public class VersionedApplication
+    {
+        private readonly VersionedApplicationState _state;
+        private int _patchNumber;
+
+        private readonly Dictionary<string, int> _buildHistory;
+
+        public VersionedApplication(VersionedApplicationState state)
+        {
+            _patchNumber = 0;
+            _state = state;
+            _buildHistory = new Dictionary<string, int>();
+        }
+
+        public string Name
+        {
+            get { return _state.ApplicationName; }
+        }
+
+        public string Description
+        {
+            get { return _state.ApplicationDescription; }
+        }
+
+        public SemVersion Version
+        {
+            get { return _state.CurrentVersion; }
+        }
+
+        public void RecordBuild(string versionControlChangeIdentifier)
+        {
+            if(!_buildHistory.ContainsKey(versionControlChangeIdentifier))
+            {
+                _buildHistory.Add(versionControlChangeIdentifier, 1);
+                _patchNumber ++;
+            }
+            else
+                _buildHistory[versionControlChangeIdentifier]++;
+            
+            _state.CurrentVersion = _state.CurrentVersion.Change(build: _buildHistory[versionControlChangeIdentifier].ToString());
+            _state.CurrentVersion = _state.CurrentVersion.Change(patch: _patchNumber);
+        }
+    }
+
+    public class ExistingVersionedApplication : VersionedApplication
+    {
+        public ExistingVersionedApplication(VersionedApplicationState state) : base(state)
+        {
+        }
+    }
+
+    public class NullVersionedApplication : VersionedApplication
+    {
+        public NullVersionedApplication()
+            : base(new VersionedApplicationState(string.Empty, string.Empty))
+        {
+        }
+
+        public new void RecordBuild(string versionControlChangeIdentifier)
+        {
+        }
+    }
+
+    public class VersionKeeper
+    {
+        private readonly IVersionApplicationStateStore _stateStore;
+
+        public VersionKeeper(IVersionApplicationStateStore stateStore)
+        {
+            _stateStore = stateStore;
+        }
+
+        public VersionedApplication AddVersionedApplication(string applicationIdentifier, string applicationDescription)
+        {
+            var existingApplication = _stateStore.Read(applicationIdentifier);
+
+            if (existingApplication != null)
+                return new ExistingVersionedApplication(existingApplication);
+
+            var versionedApplicationState = new VersionedApplicationState(applicationIdentifier, applicationDescription);
+            versionedApplicationState.CurrentVersion = new SemVersion(new Version());
+            _stateStore.Write(versionedApplicationState);
+            return new VersionedApplication(versionedApplicationState);
+        }
+
+        public VersionedApplication GetVersionedApplication(string applicationName)
+        {
+            var state = _stateStore.Read(applicationName);
+            if (state == null)
+                return new NullVersionedApplication();
+
+            return new VersionedApplication(state);
+        }
+
+        public VersionedApplication AddVersionedApplication(string applicationIdentifier, string applicationDescription,
+            SemVersion startingVersion)
+        {
+            var versionedApplicationState = new VersionedApplicationState("", "");
+            versionedApplicationState.CurrentVersion = startingVersion;
+            return new VersionedApplication(versionedApplicationState);
+        }
+    }
+
+    public interface IVersionApplicationStateStore
+    {
+        void Write(VersionedApplicationState state);
+        VersionedApplicationState Read(string applicationName);
+    }
+
+    //[Serializable]
+    public class VersionedApplicationState
+    {
+        public string ApplicationName { get; private set; }
+        public string ApplicationDescription { get; private set; }
+        public SemVersion CurrentVersion { get; set; }
+
+        public VersionedApplicationState(string applicationName, string applicationDescription)
+        {
+            ApplicationName = applicationName;
+            ApplicationDescription = applicationDescription;
+        }
+    }
+}
